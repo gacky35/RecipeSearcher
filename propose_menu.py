c852f9115521b7fab88cb8c915ce23a2a733b71b
gacky35 <s21g470@stu.kagawa-u.ac.jp>
2022-06-18 00:59:19 +0900

    correspond to float data

--- propose_menu.py
+++ propose_menu.py
@@ -9,10 +9,10 @@ import pandas as pd
 # wv = cr.crawl
 knapsack = pd.DataFrame({"ingredient": ["大根", "にんじん", "卵", "豚バラ肉", "キャベツ", "塩", "酢", "砂糖", "みりん", "しょうゆ", "黒こしょう", "ワカメ", "鶏ガラスープの素", "ゴマ油", "塩こしょう", "ごはん", "コンソメ顆粒", "酒"], "amount": [900, 150, 2, 300, 150, 100, 100, 100, 100, 100, 20, 5, 300, 100, 100, 500, 200, 100]})
 data_list = [
-    [0, 0, 1, 0, 50, 45, 0, 0, 0, 0, 45, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 5, 0, 2, 10, 5, 45, 0, 0, 0],
-    [0, 0, 2, 150, 0, 45, 0, 5, 15, 15, 0, 0, 0, 0, 0, 200, 0, 0],
-    [50, 50, 0, 0, 0, 45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0],
+    [0, 0, 1, 0, 50, 0.45, 0, 0, 0, 0, 0.45, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 5, 0, 2, 10, 5, 0.45, 0, 0, 0],
+    [0, 0, 2, 150, 0, 0.45, 0, 5, 15, 15, 0, 0, 0, 0, 0, 200, 0, 0],
+    [50, 50, 0, 0, 0, 0.45, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0],
     [400, 0, 0, 200, 0, 0, 0, 30, 30, 30, 0, 0, 0, 15, 0, 0, 0, 30]
 ]
 wv = pd.DataFrame(data_list, columns=["大根", "にんじん", "卵", "豚バラ肉", "キャベツ", "塩", "酢", "砂糖", "みりん", "しょうゆ", "黒こしょう", "ワカメ", "鶏ガラスープの素", "ゴマ油", "塩こしょう", "ごはん", "コンソメ顆粒", "酒"])
@@ -47,25 +47,24 @@ def calc_knapsack(i, w, wv, knapsack, choice):
     if (i >= len(wv)):
         return 0, ''
     elif w - sum(wv.iloc[i].values) < 0 or any(knapsack < wv.iloc[i].values):
-        return calc_knapsack(i+1, w, wv, knapsack, choice)
+        return calc_knapsack(i+1, w, wv, knapsack, choice + '0')
     else:
-        p1, c1 = calc_knapsack(i+1, w, wv, knapsack, choice)
-        data = calc_knapsack(i+1, w - sum(wv.iloc[i].values), wv, knapsack - wv.iloc[i].values, choice)
-        p2 = data[0] + sum(wv.iloc[i].values)
+        p1, c1 = calc_knapsack(i+1, w, wv, knapsack, choice + '0')
+        p2, c2 = calc_knapsack(i+1, w - sum(wv.iloc[i].values), wv, knapsack - wv.iloc[i].values, choice + '1')
+        p2 += sum(wv.iloc[i].values)
         if p1 > p2:
-            print(i, '0')
-            return p1, c1 + '0'
+            return p1, c1 + str(i)
         else:
-            print(i, '1')
-            return p2, data[1] + '1'
+            return p2, c2 + str(i)
 # %%
 p = calc_knapsack(0, sum(knapsack["amount"]), wv, knapsack["amount"], '')
 print(p)
 # %%
 knapsack["amount"] - wv.iloc[0].values
 # %%
-df_sample = pd.DataFrame([[0] * 18])
-for i in range(1, len(wv)):
-    df_sample += wv.iloc[i].values
-print(sum(df_sample.iloc[0]))
-print(knapsack["amount"] - df_sample.iloc[0].values)
+df_sample = wv.iloc[1].values + wv.iloc[3].values + wv.iloc[4].values
+print(sum(df_sample))
+# for i in range(1, len(wv)):
+#     df_sample += wv.iloc[i].values
+# print(sum(df_sample.iloc[0]))
+# print(knapsack["amount"] - df_sample.iloc[0].values)
